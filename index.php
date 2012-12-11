b2bd58190e86889b80b3a18e0645a430eee5cd2f
rafaqueque <c.rafael.s.albuquerque@gmail.com>
2012-12-11 02:57:47 +0000

    remove comments

--- index.php
+++ index.php
@@ -3,21 +3,18 @@
   <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# 
                   website: http://ogp.me/ns/website#">
     <meta charset="UTF-8" />
-    
-    <!-- Open Graph -->
     <meta property="og:type" content="blog"> 
     <meta property="og:url" content="http://canileave.com/"> 
     <meta property='og:title' content='Can I leave my home safely? — Real-time Weather Report' />
     <meta property='og:site_name' content='Can I leave my home safely? — Real-time Weather Report' />
     <meta property='og:description' content='Real-time weather report.' />
     <meta property="og:image" content="http://canileave.com/img/cloud.jpg">
-
-    <!-- SEO -->
     <meta name="description" content="Real-time weather report.">
     <meta name="keywords" content="weather, real time, live, weather report, climate, report">
     <title>Can I leave my home safely? — Real-time Weather Report</title>
 
     <link href="css/style.css" rel="stylesheet" media="screen">
+    <script src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
   </head>
   <body>
     
@@ -26,15 +23,15 @@
     </div>
     <div style='clear:both'></div>
 
+
     <div class='container'>
-      
       <form method='get'>
         Hey <input type='text' class='input-big' name='place' placeholder='city...' value='<?php echo $_GET['place']; ?>'>, can I leave my home safely?
       </form>
       
       <span class='small-text'>#protip: type the city name above and press enter.</span><br>
 
-      <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://canileave.com" data-text="Can I leave my home safely? — Weather Report —">Tweet</a>
+      <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://canileave.com" data-text="Can I leave my home safely? — Real-time Weather Report —">Tweet</a>
       <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
       <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fcanileave.com&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
@@ -43,9 +40,9 @@
 
       <?php 
 
-        if ($_GET)
+        if (isset($_GET['place']))
         {
-            include("lib/simpleweather.class.php");
+        	include("lib/simpleweather.class.php");
 
             $weather = new SimpleWeather(array("place" => $_GET['place'], "degrees" => "c"));
 
